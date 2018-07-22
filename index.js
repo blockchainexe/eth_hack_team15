073c5d5a3d42e33c26ced2c98c6e77b5abe8aa24
chanpu <chanpu.alter@gmail.com>
2018-07-22 11:28:08 +0900

    Add verify

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const EmailVerifier = require('uport-verify-email').default
 const bodyParser = require('body-parser')
 const jwtDecode = require('jwt-decode')
 const url = require('url')
-
-if (process.env.NODE_ENV != 'production') {
   require('dotenv').config()
-}
 
 const appName = process.env.APP_NAME
 const address = process.env.ADDRESS
@@ -35,32 +32,37 @@ const verifier = new EmailVerifier({
   attestationSubject: 'uPort Email Attestation',
   attestationTemplate: qr => `<html>...${qr}...</html>`,
   customRequestParams: {},
-  qrFolder: './tmp'
+  qrFolder: '/tmp/'
 })
 
 app.use(bodyParser.json({ strict: false }))
 
 app.set('view engine', 'ejs')
 
+app.options("/*", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  const allowHeader = req.header("Access-Control-Request-Headers");
+  res.header("Access-Control-Allow-Headers", allowHeader ? allowHeader : "*");
+  res.send(200);
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/register', function (req, res) {
   res.header('Access-Control-Allow-Origin', '*')
-
+  console.log(req.body.email)
   const email = req.body.email
   const requestToken = verifier.receive(email)
 
   res.json({ msg: 'success' })
 })
 
-app.get('/callback', function (req, res) {
-  res.render('pages/callback')
-})
 
 app.post('/verify', function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
 
-  const accessToken = req.body[1].value
+  const accessToken = req.body.access_token;
 
   const requestToken = jwtDecode(accessToken).req
   const callbackUrlWithEmail = jwtDecode(requestToken).callback
